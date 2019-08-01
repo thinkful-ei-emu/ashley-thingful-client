dc8aa922e4071d70672d5d5f4959677f23c93dc0
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-01 14:35:01 -0400

    implemented eventhandler for registration, fixed return of rejected promise, now error message is updated in state and renders on page when user registartion fails validation check.

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,5 +1,6 @@
 import config from '../config'
 
+
 const AuthApiService = {
   postLogin(credentials) {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
@@ -11,9 +12,20 @@ const AuthApiService = {
     })
       .then(res =>
         (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
+        ? res.json().then(e => Promise.reject(e)): res.json()
       )
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then(res => 
+      (!res.ok)
+      ? res.json().then(e => Promise.reject(e)) : res.json())
   },
 }
 
